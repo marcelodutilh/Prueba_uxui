0e36e32861744b62e8e0d89b0e7d6643918b0bf9
marcelodutilh <mdutilh@uc.cl>
2019-10-08 23:19:29 -0300

    version final 1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,16 @@ $(document).ready(function () {
         $('[data-toggle="popover"]').popover()
     })
 
+    $('a[href*="#"]').on('click', function(e) {
+        e.preventDefault()
+      
+        $('html, body').animate(
+          {
+            scrollTop: $($(this).attr('href')).offset().top,
+          },
+          500,
+          'linear'
+        )
+      })
 });
+
